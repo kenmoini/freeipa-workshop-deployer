ff449c97112222ba82560baff826ba459f3bfaf6
Tosin Akinosho <takinosh@redhat.com>
2023-03-17 11:52:06 -0400

    updatng code

--- 1_kcli/create.sh
+++ 1_kcli/create.sh
@@ -57,7 +57,7 @@ cd /opt/qubinode-installer/kcli-plan-samples
 ansiblesafe -f "${ANSIBLE_VAULT_FILE}" -o 2
 PASSWORD=$(yq eval '.admin_user_password' "${ANSIBLE_VAULT_FILE}")
 RHSM_ORG=$(yq eval '.rhsm_org' "${ANSIBLE_VAULT_FILE}")
-RHSM_ACTIVATION_KEY=$(yq eval '.admin_user_password' "${ANSIBLE_VAULT_FILE}")
+RHSM_ACTIVATION_KEY=$(yq eval '.rhsm_activationkey' "${ANSIBLE_VAULT_FILE}")
 sudo python3 profile_generator/profile_generator.py update_yaml freeipa rhel9/template.yaml --image rhel-baseos-9.1-x86_64-kvm.qcow2 --user $USER --user-password ${PASSWORD} --rhnorg ${RHSM_ORG} --rhnactivationkey ${RHSM_ACTIVATION_KEY}
 cat  kcli-profiles.yml
 cp kcli-profiles.yml ${KCLI_CONFIG_DIR}/profiles.yml
