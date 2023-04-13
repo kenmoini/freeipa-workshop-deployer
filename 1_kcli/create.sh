ef07f1e0871bf1301d7da80b3d575867d9b677ce
Tosin Akinosho <takinosh@redhat.com>
2023-04-13 16:39:36 -0400

    updating code

--- 1_kcli/create.sh
+++ 1_kcli/create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+#export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 set -xe	## Uncomment for debugging
 
 if [ "$EUID" -ne 0 ]
